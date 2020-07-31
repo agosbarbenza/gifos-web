67fd9839a1765bcae6e7e553663ee2b7078b4957
w Agostina Barbenza <agosbarbenza@gmail.com>
2020-07-31 12:17:31 -0300

    correciones de organizacion de variables

--- js/camscripts.js
+++ js/camscripts.js
@@ -1,112 +1,100 @@
 
-//Video
+//Variables Globales
+//*****Gif ID
+let gifId
+//*****Guardar gifo
+let form = new FormData();
+//*****Video 
+let blob;
+let grabacion;
 let video = document.getElementById("video");
 let constraintObj = { //Lo que quiero obtener del getUserMedia
     audio: false,
     video: {
-        height: { 
-            max: 480 
-        }}
-}; 
-let blob
-
-//Guardar gifo
-let form = new FormData();
-
-let vidContainer = document.getElementById("vidContainer");
-let vistaPreviaContainer = document.getElementById("vistaPreviaContainer");
-
-//Botones
-let btnStart = document.getElementById("btn-start");
+        height: {
+            max: 480
+        }
+    }
+};
 
 
-//Botones capturar
-let btnCamCapturar = document.getElementById("btn1")
-let btnCapturar = document.getElementById("btn2");
-let btnsCapturar = [btnCamCapturar, btnCapturar];
-let containerCapturar = document.getElementById("btns-capturar-cam");
+//////////////////////////WINDOW ON LOAD///////////////////////////////
 
-//botones listo
-let containerBtns = document.getElementById("btns-listo");
-let btnListo = document.getElementById("btn-listo");
-let btnRecordIcon= document.getElementById("record-icon");
-let btnsListo = [btnListo, btnRecordIcon];
-
-//boton repetir
-let btnRepetir = document.getElementById("btnRepetir");
-
-let btnStartRepetir = [btnStart, btnRepetir];
-
-//boton subir gifo
-let subirBtn = document.getElementById("subir-btn");
-
-//boton cancelar
-let btnCancelar = document.getElementById("btn-cancel");
-
-//boton copiar enlace
-let copiarEnlaceBtn = document.getElementById("btn-copiar-enlace");
-let descargarBtn = document.getElementById("btn-descargar-gifo");
-
-//boton mis gifos
-let misGifosBtn = document.getElementById("mis-gifos-boton");
-
-
-//Cards
-let crearGifosCard = document.getElementById("crearGifos");
-let chequeoCard = document.getElementById("chequeo");
-let vistaPreviaCard = document.getElementById("vistaPrevia");
-let subiendoCard = document.getElementById("subiendo-gifo-card");
-let exitoCard = document.getElementById("exito-card");
-let misGifosCard = document.getElementById("misGifos");
-
-//ID Gif
-let gifId
-
-window.onload = ()=>{
+window.onload = () => {
     misGifos();
-    
+
     if (localStorage.getItem("IDGif") === null) {
         document.getElementById("aun").style.display = "block";
-    }else{
+    } else {
         document.getElementById("aun").style.display = "none";
     }
+
+    getCam();
+    recordCam();
+    stopRecord();
+    subirGifo();
+    cancelarSubida();
+    copiarLink();
+    descargarGif();
 }
 
 
-function getCam(){
-    btnStartRepetir.forEach((btn)=>{
-        btn.addEventListener('click', ()=>{
+//////////////////////////OBTENER CAMARA///////////////////////////////
+
+function getCam() {
+    let btnStart = document.getElementById("btn-start");
+    let btnRepetir = document.getElementById("btnRepetir");
+    let btnStartRepetir = [btnStart, btnRepetir];
+
+    btnStartRepetir.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            let crearGifosCard = document.getElementById("crearGifos");
             crearGifosCard.style.display = "none";
+            let chequeoCard = document.getElementById("chequeo");
             chequeoCard.style.display = "block";
+            let vistaPreviaCard = document.getElementById("vistaPrevia");
             vistaPreviaCard.style.display = "none";
+            let containerBtns = document.getElementById("btns-listo");
             containerBtns.style.display = "none";
+            let containerCapturar = document.getElementById("btns-capturar-cam");
             containerCapturar.style.display = "flex";
             navigator.mediaDevices.getUserMedia(constraintObj)//método para acceder a la cámara
-                .then(function(mediaStreamObj){ //mediaStreamObj es lo que obtengo
-                    grabacion = mediaStreamObj; 
+                .then(function (mediaStreamObj) { //mediaStreamObj es lo que obtengo
+                    grabacion = mediaStreamObj;
                     video.srcObject = mediaStreamObj; // relleno la etiqueta video con lo que obtuve
                     video.play() //comienza la cámara
-                    })
-                .catch(error =>{
-            console.log(error); 
-        })
+                })
+                .catch(error => {
+                    console.log(error);
+                })
         })
     })
-    
+
 }
 
-getCam()
+/*getCam()*/
+
+//////////////////////////GRABAR GIF///////////////////////////////
 
 let grabando;
 
-function recordCam(){
-    btnsCapturar.forEach((btn)=>{
-        btn.addEventListener('click', ()=>{
+function recordCam() {
+    let btnCamCapturar = document.getElementById("btn1");
+    let btnCapturar = document.getElementById("btn2");
+    let btnsCapturar = [btnCamCapturar, btnCapturar];
+
+    btnsCapturar.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            let vistaPreviaContainer = document.getElementById("vistaPreviaContainer");
             vistaPreviaContainer.innerHTML = "";
+            let containerCapturar = document.getElementById("btns-capturar-cam");
             containerCapturar.style.display = "none";
+            let containerBtns = document.getElementById("btns-listo");
             containerBtns.style.display = "flex";
             document.getElementById("titulo").innerText = "Capturando tu gifO";
+            let btnRecordIcon = document.getElementById("record-icon");
             btnRecordIcon.classList.add('record');
+            let btnListo = document.getElementById("btn-listo");
             btnListo.classList.add('record');
             recorder = RecordRTC(grabacion, {
                 type: 'gif',
@@ -114,55 +102,66 @@ function recordCam(){
                 quality: 10,
                 width: 360,
                 hidden: 240,
-                onGifRecordingStarted: function() {
-                 console.log('started')
-               },
-              });
+                onGifRecordingStarted: function () {
+                    console.log('started')
+                },
+            });
             recorder.startRecording();
             grabando = true;
-            setTimeout(() => {         
-            grabando = false;
+            setTimeout(() => {
+                grabando = false;
             }, 2000); //debe grabar mínimo 2 segundos
         })
 
     })
 
 }
-recordCam()
+/*recordCam()*/
 
-function stopRecord(){
-    btnsListo.forEach((btn)=>{
-        btn.addEventListener("click", ()=>{
-            if (grabando == false){ //si grabó mínimo 2 segundos
-        chequeoCard.style.display = "none";
-        vistaPreviaCard.style.display = "block";
-        let gif = document.createElement("img");
-        gif.style.width = "100%";
-        vistaPreviaContainer.appendChild(gif);
-        recorder.stopRecording();
-        blob = recorder.getBlob();
-        gif.setAttribute("src", URL.createObjectURL(blob));
-        vistaPreviaContainer.style.display = "block";
-        recorder.destroy(); 
-        recorder = null; 
-        grabacion.getTracks().map(function (cam) {
-            cam.stop();
-          });
-        }
+function stopRecord() {
+    let btnListo = document.getElementById("btn-listo");
+    let btnRecordIcon = document.getElementById("record-icon");
+    let btnsListo = [btnListo, btnRecordIcon];
+    btnsListo.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            if (grabando == false) { //si grabó mínimo 2 segundos
+                let chequeoCard = document.getElementById("chequeo");
+                chequeoCard.style.display = "none";
+                let vistaPreviaCard = document.getElementById("vistaPrevia");
+                vistaPreviaCard.style.display = "block";
+                let gif = document.createElement("img");
+                gif.style.width = "100%";
+                let vistaPreviaContainer = document.getElementById("vistaPreviaContainer");
+                vistaPreviaContainer.appendChild(gif);
+                recorder.stopRecording();
+                blob = recorder.getBlob();
+                gif.setAttribute("src", URL.createObjectURL(blob));
+                vistaPreviaContainer.style.display = "block";
+                recorder.destroy();
+                recorder = null;
+                grabacion.getTracks().map(function (cam) {
+                    cam.stop();
+                });
+            }
+        })
+
     })
-    
-})
 }
 
-stopRecord()
+/*stopRecord()*/
+
+//////////////////////////SUBIR GIFO///////////////////////////////
 
 let arrayGifos = [];
 let gifosContainer = document.getElementById("misGifosContainer");
 
-function subirGifo(){
-    subirBtn.addEventListener('click', ()=>{
-        vistaPreviaCard.style.display="none";        
-        subiendoCard.style.display ="block";
+function subirGifo() {
+    let subirBtn = document.getElementById("subir-btn");
+    subirBtn.addEventListener('click', () => {
+        let vistaPreviaCard = document.getElementById("vistaPrevia");
+        vistaPreviaCard.style.display = "none";
+        let subiendoCard = document.getElementById("subiendo-gifo-card");
+        subiendoCard.style.display = "block";
         espera();
         let gif = document.createElement('img');
         gif.style.width = "100%";
@@ -172,95 +171,102 @@ function subirGifo(){
         form.append('file', blob, 'myGif.gif');
         console.log(form.get('file'));
         fetch("https://upload.giphy.com/v1/gifs?api_key=oe44FnxvDSz23VKoIEmtoRbzdYL7ISE1",
-        {
-            method: "POST",
-            body: form,
-        }
-        ).then(function(response){
-            return response.json()} //pasar a json para acceder al id
-        ).then(function (response){
+            {
+                method: "POST",
+                body: form,
+            }
+        ).then(function (response) {
+            return response.json()
+        } //pasar a json para acceder al id
+        ).then(function (response) {
             console.log(response);
             gifId = response.data.id;
             console.log(gifId);
             arrayGifos = JSON.parse(localStorage.getItem("IDGif")) || []; //response está en json, hay que pasarlo a objeto. Si hay algo, pasarlo a objeto, si no hay nada, dejarlo vacío.
-            arrayGifos.unshift(gifId);  
+            arrayGifos.unshift(gifId);
             localStorage.setItem("IDGif", JSON.stringify(arrayGifos)); // convierte el array en un string.  
             document.getElementById("aun").style.display = "none";
             misGifos();
-         }).catch(function(error){
-            console.log(error+" Se produjo un error")
+        }).catch(function (error) {
+            console.log(error + " Se produjo un error");
         });
-        
+
     })
 }
 
-subirGifo()
+/*subirGifo();*/
 
-function espera(){
+function espera() {
     let barraRosa = document.getElementById("barra-rosa");
     let width = 1;
     let progress = setInterval(avanzar, 50);
-    function avanzar(){
-        if(width >= 100){
-            clearInterval(progress)
-            subiendoCard.style.display ="none";
-            exitoCard.style.display= "block";
-        }else{
+    function avanzar() {
+        if (width >= 100) {
+            clearInterval(progress);
+            let subiendoCard = document.getElementById("subiendo-gifo-card");
+            subiendoCard.style.display = "none";
+            let exitoCard = document.getElementById("exito-card");
+            exitoCard.style.display = "block";
+        } else {
             width++;
             barraRosa.style.width = width + "%";
         }
     }
 }
 
-function cancelarSubida(){
-    btnCancelar.addEventListener('click', ()=>{
+function cancelarSubida() {
+    let btnCancelar = document.getElementById("btn-cancel");
+    btnCancelar.addEventListener('click', () => {
         window.location.href = "index.html";
     })
 }
-cancelarSubida();
+/*cancelarSubida();*/
 
-document.getElementById("boton-listo-gifo-creado").addEventListener('click', ()=>{
+document.getElementById("boton-listo-gifo-creado").addEventListener('click', () => {
+    let exitoCard = document.getElementById("exito-card");
     exitoCard.style.display = "none";
-    /*misGifosCard.style.top = "10rem";*/
 })
 
 
-/*************************MIS GIFOS ***********************/
-function misGifos(){
-    gifosContainer.innerHTML=" ";
+//////////////////////////MIS GIFOS///////////////////////////////
+
+function misGifos() {
+    gifosContainer.innerHTML = " ";
     arrayGifos = JSON.parse(localStorage.getItem("IDGif")) || [];
-    if (typeof (arrayGifos[0]) == "string"){
-    for (let i = 0; i < arrayGifos.length; i++) {
-        let gif = document.createElement("img")
-        gifosContainer.appendChild(gif);
-        gif.src = "https://media.giphy.com/media/" + arrayGifos[i] + "/giphy.gif";
-        gif.style.marginTop = "1rem";
-        misGifosCard.appendChild(gifosContainer).appendChild(gif);
-}
-}
+    if (typeof (arrayGifos[0]) == "string") {
+        for (let i = 0; i < arrayGifos.length; i++) {
+            let gif = document.createElement("img")
+            gifosContainer.appendChild(gif);
+            gif.src = "https://media.giphy.com/media/" + arrayGifos[i] + "/giphy.gif";
+            gif.style.marginTop = "1rem";
+            let misGifosCard = document.getElementById("misGifos");
+            misGifosCard.appendChild(gifosContainer).appendChild(gif);
+        }
+    }
 }
 
 
 
-//'''''''''''''''''''''''FUNCIONALIDAD COPIAR'''''''''''''''''''''''''''
+//////////////////////////COPIAR GIFO///////////////////////////////
 
 let urlCopy
 let inputCopy
 
-function copiarLink(){
+function copiarLink() {
+    let copiarEnlaceBtn = document.getElementById("btn-copiar-enlace");
 
-    copiarEnlaceBtn.addEventListener('click', ()=>{
-        urlCopy = "https://media.giphy.com/media/"+gifId+"/giphy.gif";
+    copiarEnlaceBtn.addEventListener('click', () => {
+        urlCopy = "https://media.giphy.com/media/" + gifId + "/giphy.gif";
         copiar(urlCopy);
         copiarEnlaceBtn.innerText = "Enlace copiado!";
         setTimeout(() => {
             copiarEnlaceBtn.innerText = "Copiar Enlace Gifo";
-          }, 1500);
-     
+        }, 1500);
+
     })
 }
 
-function copiar(urlacopiar){
+function copiar(urlacopiar) {
     inputCopy = document.createElement("textarea");
     document.getElementById("divInput").appendChild(inputCopy);
     inputCopy.value = urlacopiar;
@@ -269,19 +275,21 @@ function copiar(urlacopiar){
     document.getElementById("divInput").removeChild(inputCopy);
 }
 
-copiarLink()
+/*copiarLink();*/
 
-//''''''''''''''''''FUNCIONALIDAD DESCARGAR''''''''''''''''''''''''''''
 
-function descargarGif(){
-    descargarBtn.addEventListener('click', ()=>{
+//////////////////////////DESCARGAR GIFO///////////////////////////////
+
+function descargarGif() {
+    let descargarBtn = document.getElementById("btn-descargar-gifo");
+    descargarBtn.addEventListener('click', () => {
         let descarga = document.createElement('a');
         document.getElementById("divInput").appendChild(descarga);
-        descarga.setAttribute('href',URL.createObjectURL(blob));
+        descarga.setAttribute('href', URL.createObjectURL(blob));
         descarga.setAttribute('download', 'mygif.gif');
         document.getElementById("divInput").removeChild(descarga);
         descarga.click(); //simula el click del 'a'
-    })    
+    })
 }
 
-descargarGif()
+/*descargarGif();*/
