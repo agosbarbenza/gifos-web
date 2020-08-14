cd6b00d63f3fee15da9380f319252afecce38ce0
w Agostina Barbenza <agosbarbenza@gmail.com>
2020-08-14 00:39:49 -0300

    retoques en manejo de temas, correcion de colores en botones y posiciones

--- js/temascripts.js
+++ js/temascripts.js
@@ -8,6 +8,9 @@ let sailorContainer = document.getElementById("sailor-container");
 let sailorDay = document.getElementById("day");
 let sailorNight = document.getElementById("night");
 
+let cambiarEstilo = document.querySelector(".probar");
+let button2 = document.querySelector(".button2");
+
 
 document.getElementById("night").addEventListener("click", function () {
     temaDark();
@@ -21,11 +24,15 @@ document.getElementById("day").addEventListener("click", function () {
 function temaDark(){
     document.getElementById("themes").setAttribute("href", "./css/dark.css");
     sessionStorage.setItem("theme", "dark");
+    button2.style.background = "#EE3EFE";
+    halfButton.style.background = "#EE3EFE"
 }
 
 function temaDay() {
     document.getElementById("themes").setAttribute("href", "./css/styles.css");
     sessionStorage.setItem("theme", "day");
+    button2.style.background = "#F7C9F3";
+    halfButton.style.background = "#F7C9F3";
 }
 
 cambiarTema();
@@ -43,23 +50,33 @@ function cambiarTema() {
 
 /////////////////Estilo dinámico del botón cambiar tema
 
-let cambiarEstilo = document.querySelector(".probar");
-let button2 = document.querySelector(".button2");
+
 
 cambiarEstilo.addEventListener('mouseover', ()=> {
-        button2.style.border = "1px dashed #110038";
+    button2.style.border = "1px dashed #110038";
         halfButton.style.border = "1px dashed #110038";
         button2.style.boxshadow = "inset -1px -1px 0 0 #997D97, inset 1px 1px 0 0 #FFFFFF";
         halfButton.style.boxshadow = "inset -1px -1px 0 0 #997D97, inset 1px 1px 0 0 #FFFFFF";
+
+    if(sessionStorage.getItem("theme") == "day"){ 
         button2.style.background = "#E6BBE2";
         halfButton.style.background = "#E6BBE2";
+    }else{
+        button2.style.background = "#CE36DB";
+        halfButton.style.background = "#CE36DB";
+    }
 })
 
 cambiarEstilo.addEventListener('mouseout', ()=> {
     button2.style.border = "1px solid #110038";
     halfButton.style.border = "1px solid #110038";
+    if(sessionStorage.getItem("theme") == "day"){
     button2.style.background = "#F7C9F3";
     halfButton.style.background = "#F7C9F3";
+    }else{
+        button2.style.background = "#EE3EFE";
+        halfButton.style.background = "#EE3EFE";
+    }
 })
 
 
